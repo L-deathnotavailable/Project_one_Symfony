ae42076cc45f7438609d22fdf145777246c105cc
laurie <lauriegauth@gmail.com>
2025-06-27 16:02:53 +0200

    ajout des éléments manquants pour l'attribut Voiture

--- src/Entity/Voiture.php
+++ src/Entity/Voiture.php
@@ -20,6 +20,18 @@ class Voiture
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
+    #[ORM\Column]
+    private ?float $prixQuotidien = null;
+
+    #[ORM\Column(length: 255)]
+    private ?float $prixMensuel = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $places = null;
+
+    #[ORM\Column]
+    private ?bool $manuelle = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -45,6 +57,54 @@ class Voiture
     public function setDescription(string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrixQuotidien(): ?float
+    {
+        return $this->prixQuotidien;
+    }
+
+    public function setPrixQuotidien(float $prixQuotidien): static
+    {
+        $this->prixQuotidien = $prixQuotidien;
+
+        return $this;
+    }
+
+    public function getPrixMensuel(): ?string
+    {
+        return $this->prixMensuel;
+    }
+
+    public function setPrixMensuel(string $prixMensuel): static
+    {
+        $this->prixMensuel = $prixMensuel;
+
+        return $this;
+    }
+
+    public function getPlaces(): ?string
+    {
+        return $this->places;
+    }
+
+    public function setPlaces(string $places): static
+    {
+        $this->places = $places;
+
+        return $this;
+    }
+
+    public function isManuelle(): ?bool
+    {
+        return $this->manuelle;
+    }
+
+    public function setManuelle(bool $manuelle): static
+    {
+        $this->manuelle = $manuelle;
 
         return $this;
     }
