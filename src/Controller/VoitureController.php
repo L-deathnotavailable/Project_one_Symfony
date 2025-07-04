f959df985804271622af00db9ee2d08ce5c93117
laurie <lauriegauth@gmail.com>
2025-07-04 11:34:10 +0200

    Mise en place de la partie voiture en twig, de l'accueil ainsi que la base

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\VoitureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class VoitureController extends AbstractController
+class VoitureController extends AbstractController
 {
-    #[Route('/', name: 'app_voiture')]
-    public function index(): Response
+    #[Route('/', name: 'accueil')]
+    public function accueil(VoitureRepository $voitureRepository): Response
     {
-        return $this->render('voiture/index.html.twig', [
-            'controller_name' => 'VoitureController',
+        $voitures = $voitureRepository->findAll();
+
+        return $this->render('accueil.html.twig', [
+            'voitures' => $voitures,
         ]);
     }
 }
